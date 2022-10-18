b1104311b354e2502fdc4e2d5b6bc528ed775dd9
rtexelm <texelist@gmail.com>
2022-10-18 16:26:57 -0400

    Passed spec 00

--- build_active_record/lib/00_attr_accessor_object.rb
+++ build_active_record/lib/00_attr_accessor_object.rb
@@ -2,11 +2,11 @@ class AttrAccessorObject
   def self.my_attr_accessor(*names)
     names.each do |method_name|
 
-      define_method(method_name) do |method|
-        self.instance_variable_get(:method_name)
+      define_method(method_name) do 
+        self.instance_variable_get("@#{method_name}")
       end
 
-      define_method("@method_name=") do |setter|
+      define_method("#{method_name}=") do |setter|
         self.instance_variable_set("@#{method_name}", setter)
       end
     end
