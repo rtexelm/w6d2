772413d8e5bf3fc4bb60d655a3267d6792b09ef5
rtexelm <texelist@gmail.com>
2022-10-18 16:06:13 -0400

    Started spec 00

---
+++ build_active_record/lib/00_attr_accessor_object.rb
@@ -0,0 +1,14 @@
+class AttrAccessorObject
+  def self.my_attr_accessor(*names)
+    names.each do |method_name|
+
+      define_method(method_name) do |method|
+        self.instance_variable_get(:method_name)
+      end
+
+      define_method("@method_name=") do |setter|
+        self.instance_variable_set("@#{method_name}", setter)
+      end
+    end
+  end
+end
